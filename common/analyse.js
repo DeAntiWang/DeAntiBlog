2dfa342f7ece6717f8024d4435d43d6f02d0f505
DarkKris <long4664030@163.com>
2020-04-02 10:48:12 +0800

    fix: fix analyse

--- common/analyse.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?d790d9f864c6f0f4152f1186bb72c282";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
