69dd862f59fbb4f575d00777b7d83351d1edb8eb
DeAntiWang <long4664030@163.com>
2023-07-20 00:27:57 +0800

    index page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'DeAnti Blog';
+export const SITE_DESCRIPTION = '日摇圈核狗程序员。偶尔搞开发，偶尔弹弹琴，偶尔听音乐。万年先锋卫士刀斯林。';
