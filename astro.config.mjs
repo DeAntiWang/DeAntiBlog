65f370c1dee53856013c9beb768071d726b09798
deantiwang <long4664030@163.com>
2023-07-25 00:25:52 +0800

    pagination component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'http://deanti.wang',
+	integrations: [vue(), mdx(), sitemap()],
 	experimental: {
 		assets: true
 	},
